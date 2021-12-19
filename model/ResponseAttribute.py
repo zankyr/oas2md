a6040c46596c61084e40d2280c8c77fd8cba0a93
rik <p3mal88@gmail.com>
2021-12-19 12:30:49 +0100

    Rename classes

--- model/ResponseAttribute.py
+++
@@ -1,9 +0,0 @@
-from dataclasses import dataclass, field
-
-@dataclass
-class ResponseAttribute():
-    name: str
-    description: str = field(init=False, default=None)
-    type: str = field(init=False, default=None)
-    required: bool = field(init=False, default=False)
-    example: str = field(init=False, default=None)
--- model/ResponseBody.py
+++
@@ -1,8 +0,0 @@
-from dataclasses import dataclass
-
-from model.ResponseAttribute import ResponseAttribute
-
-@dataclass
-class ResponseBody():
-    media_type: str
-    attributes: list[ResponseAttribute]
